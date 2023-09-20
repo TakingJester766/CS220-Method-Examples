69fd4228fcd6b29ef104f23480e853f0488f74ed
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-09-20 11:38:21 -0400

    filter

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
-console.log('Hello World!');
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredNumbers = numbers.filter((num) => {
+    return num % 2 !== 0;
+});
+console.log(filteredNumbers); // Output: [1, 3, 5, 7, 9]
 export {};
 //# sourceMappingURL=index.js.map
